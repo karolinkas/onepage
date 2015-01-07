2a7b2258d64fe6ef9cd36c16f8cbb8d4778c86c3
Karolinka <li-nee@gmx.de>
2015-01-07 12:34:47 +0100

    registerform

--- assets/javascripts/navigation.js
+++ assets/javascripts/navigation.js
@@ -1,31 +1,37 @@
 window.onload = function() {
 
-	// var featurbuttonHtml = $(".featureButton:nth-child(1)").html();
-	// featurbuttonHtml.style("margin-left:-20px;");
+    // var featurbuttonHtml = $(".featureButton:nth-child(1)").html();
+    // featurbuttonHtml.style("margin-left:-20px;");
 
-	$("#tv,#tablestuff,#buttoncontainer,#LoginContainer").hide();
+    $("#tv,#tablestuff,#buttoncontainer,#LoginContainer").hide();
+    $("#RegisterContainer").hide();
 
-	$(".icon:nth-child(1)").click(function(){
-		$("#iconcontainer").hide();
-	});
+    $(".icon:nth-child(1)").click(function() {
+        $("#iconcontainer").hide();
+    });
 
-	$(".icon:nth-child(2)").click(function(){
-		$("#iconcontainer").hide();
-		$("#tv,#tablestuff,#buttoncontainer").show();
-	});
+    $(".icon:nth-child(2)").click(function() {
+        $("#iconcontainer").hide();
+        $("#tv,#tablestuff,#buttoncontainer").show();
+    });
 
-	$(".icon:nth-child(3)").click(function(){
-		$("#iconcontainer").hide();
-	});
+    $(".icon:nth-child(3)").click(function() {
+        $("#iconcontainer").hide();
+    });
 
-	$("#logo").click(function(){
-		$("#tv,#tablestuff,#buttoncontainer,#LoginContainer").hide();
-		$("#iconcontainer").show();
-	});
+    $("#logo").click(function() {
+        $("#tv,#tablestuff,#buttoncontainer,#LoginContainer").hide();
+        $("#iconcontainer").show();
+    });
 
-	$("#LoginButton").click(function(){
-		$("#iconcontainer,#tv,#tablestuff,#buttoncontainer").hide();
-		$("#LoginContainer").show();
-	});
+    $("#LoginButton").click(function() {
+        $("#iconcontainer,#tv,#tablestuff,#buttoncontainer").hide();
+        $("#LoginContainer").show();
+    });
+
+     $("#RegisterButton").click(function() {
+     		$("#LoginContainer").hide();
+     		$("#RegisterContainer").show();
+     });
 
 };
