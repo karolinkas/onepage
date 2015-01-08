8b7f6728743c4add312bfeee66a2c8fff9b012c3
Karolinka <li-nee@gmx.de>
2015-01-08 17:22:17 +0100

    first frontend

--- assets/javascripts/navigation.js
+++ assets/javascripts/navigation.js
@@ -1,37 +1,55 @@
+
+var currentView;
+
+var HOME_VIEW;
+var LOGIN_VIEW;
+var REGISTER_VIEW;
+var GAME_VIEW;
+
 window.onload = function() {
+    HOME_VIEW = $("#HomeView");
+    LOGIN_VIEW = $("#LoginView");
+    REGISTER_VIEW = $("#RegisterView");
+    GAME_VIEW = $("#GameView");
 
-    // var featurbuttonHtml = $(".featureButton:nth-child(1)").html();
-    // featurbuttonHtml.style("margin-left:-20px;");
+    currentView = HOME_VIEW;
+    currentView.css({left:"50%"});
 
-    $("#tv,#tablestuff,#buttoncontainer,#LoginContainer").hide();
-    $("#RegisterContainer").hide();
+    $("img:nth-child(1)").click();
+    $("img:nth-child(2)").click(function () {nextView(GAME_VIEW)});
+    $("img:nth-child(3)").click();
 
-    $(".icon:nth-child(1)").click(function() {
-        $("#iconcontainer").hide();
-    });
+    $("#logo").click(function () {nextView(HOME_VIEW)});
+		$("#LoginButtonContainer").click(function () {nextView(LOGIN_VIEW)});
+		$("#RegisterButton").click(function () {nextView(REGISTER_VIEW)});
+    $(".BackButton").click(function () {nextView(HOME_VIEW)});
+    
 
-    $(".icon:nth-child(2)").click(function() {
-        $("#iconcontainer").hide();
-        $("#tv,#tablestuff,#buttoncontainer").show();
-    });
-
-    $(".icon:nth-child(3)").click(function() {
-        $("#iconcontainer").hide();
-    });
-
-    $("#logo").click(function() {
-        $("#tv,#tablestuff,#buttoncontainer,#LoginContainer").hide();
-        $("#iconcontainer").show();
-    });
-
-    $("#LoginButton").click(function() {
-        $("#iconcontainer,#tv,#tablestuff,#buttoncontainer").hide();
-        $("#LoginContainer").show();
-    });
-
-     $("#RegisterButton").click(function() {
-     		$("#LoginContainer").hide();
-     		$("#RegisterContainer").show();
-     });
+    $( window ).resize(resize);
+    resize();
 
 };
+
+function resize() {
+	var h = $( window ).height();
+	var y = (h - HOME_VIEW.height()) * .5;
+	LOGIN_VIEW.css({top:y});
+
+	y = (h - REGISTER_VIEW.height()) * .5;
+	REGISTER_VIEW.css({top:y});
+}
+
+function nextView(view) {
+	console.log("NEXT VIEW = " + view);
+	if (currentView) {
+		TweenMax.to(currentView, 0.7, {css:{left:"-50%"}, ease:Quad.easeInOut});
+	}
+	
+	currentView = view;
+	currentView.css({left:"150%"});
+	TweenMax.to(currentView, 0.7, {css:{left:"50%"}, ease:Quad.easeInOut});
+
+	
+}
+
+
